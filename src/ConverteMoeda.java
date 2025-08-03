c988044bb2387de418b663a2664debd8598cdecb
rickoshi <145235121+rickoshi@users.noreply.github.com>
2025-08-02 21:03:34 -0300

    Change user input from String to int

--- src/ConverteMoeda.java
+++ src/ConverteMoeda.java
@@ -1,26 +1,37 @@
 public class ConverteMoeda {
 
-    public void converteMoeda(Moeda moeda, String codigoMoeda, double valor) {
-
-        System.out.println("A moeda escolhida foi: ");
-        System.out.println(codigoMoeda);
+    public void converteMoeda(Moeda moeda, int codigoMoeda, double valor) {
+        System.out.println("A moeda será convertida para: ");
+        System.out.println(retornaCodigoMoedaString(codigoMoeda));
         System.out.println();
 
-        double valorMoedaConvertida = escolheMoeda(moeda.conversion_rates(), codigoMoeda) * valor;
+        double valorMoedaConvertida = retornaValorMoeda(moeda.conversion_rates(), codigoMoeda) * valor;
         System.out.println("Taxa de conversão: ");
         System.out.println(valorMoedaConvertida);
         System.out.println();
     }
 
-    public double escolheMoeda(MoedaTaxaConversao moedaTaxaConversao, String codigoMoeda) {
+    public double retornaValorMoeda(MoedaTaxaConversao moedaTaxaConversao, int codigoMoeda) {
         return switch (codigoMoeda) {
-            case "ARS" -> moedaTaxaConversao.ARS();
-            case "BOB" -> moedaTaxaConversao.BOB();
-            case "BRL" -> moedaTaxaConversao.BRL();
-            case "CLP" -> moedaTaxaConversao.CLP();
-            case "COP" -> moedaTaxaConversao.COP();
-            case "USD" -> moedaTaxaConversao.USD();
+            case 1 -> moedaTaxaConversao.ARS();
+            case 2 -> moedaTaxaConversao.BOB();
+            case 3 -> moedaTaxaConversao.BRL();
+            case 4 -> moedaTaxaConversao.CLP();
+            case 5 -> moedaTaxaConversao.COP();
+            case 6 -> moedaTaxaConversao.USD();
             default -> -1;
+        };
+    }
+
+    public String retornaCodigoMoedaString(int codigoMoeda) {
+        return switch (codigoMoeda) {
+            case 1 -> "ARS";
+            case 2 -> "BOB";
+            case 3 -> "BRL";
+            case 4 -> "CLP";
+            case 5 -> "COP";
+            case 6 -> "USD";
+            default -> "";
         };
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -5,39 +5,46 @@ public class Main {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite a String para converter a partir de: ");
+        // Create the object to convert Moeda
+        // The object can convert and print Moeda, return the value of Moeda, and return the String of Moeda
+        ConverteMoeda converteMoeda = new ConverteMoeda();
+
+        System.out.println("Digite o número para converter a partir de: ");
         System.out.println("1) ARS");
         System.out.println("2) BOB");
         System.out.println("3) BRL");
         System.out.println("4) CLP");
         System.out.println("5) COP");
         System.out.println("6) USD");
-        String codigoMoedaInicial = scanner.nextLine().toUpperCase();
+        int codigoMoedaInicial = scanner.nextInt();
         System.out.println();
 
         // The base currency is set in the second to last parameter and the target currency is set in the last parameter
-        String endereco = "https://v6.exchangerate-api.com/v6/6b46ad142b98f85a3b33e80b/latest/" + codigoMoedaInicial;
+        String endereco = "https://v6.exchangerate-api.com/v6/6b46ad142b98f85a3b33e80b/latest/" +
+                converteMoeda.retornaCodigoMoedaString(codigoMoedaInicial);
 
         // Create the object to request and return Moeda
         ConsultaMoeda consultaMoeda = new ConsultaMoeda();
         Moeda novaMoeda = consultaMoeda.consultaMoeda(endereco);
 
-        System.out.println("Digite a String para converter a partir de " + codigoMoedaInicial + ": ");
+        System.out.println("Digite o número para converter a partir de " +
+                converteMoeda.retornaCodigoMoedaString(codigoMoedaInicial) + " para: ");
         System.out.println("1) ARS");
         System.out.println("2) BOB");
         System.out.println("3) BRL");
         System.out.println("4) CLP");
         System.out.println("5) COP");
         System.out.println("6) USD");
-        String codigoMoedaFinal = scanner.nextLine().toUpperCase();
+        int codigoMoedaFinal = scanner.nextInt();
         System.out.println();
 
         System.out.println("Digite o valor para ser convertido: ");
         int valorUsuario = scanner.nextInt();
         System.out.println();
 
-        // Create the object to convert Moeda
-        ConverteMoeda converteMoeda = new ConverteMoeda();
+        System.out.println("A moeda inicial é: ");
+        System.out.println(converteMoeda.retornaCodigoMoedaString(codigoMoedaInicial));
+        System.out.println();
         converteMoeda.converteMoeda(novaMoeda, codigoMoedaFinal, valorUsuario);
     }
 }
